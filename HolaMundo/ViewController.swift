2652da976a074d2909740ecd5c9c7ccb4f988676
Dmunoz94 <davidandresmunoztrevino@gmail.com>
2015-10-15 02:48:19 -0500

    Initial Commit

---
+++ HolaMundo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HolaMundo
+//
+//  Created by David Munoz Trevino on 15/10/15.
+//  Copyright © 2015 David Munoz Trevino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
